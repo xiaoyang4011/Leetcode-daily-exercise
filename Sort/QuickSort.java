5eae458e820cd77c84ea579844e2ba8f7e2aa592
Shawn <liuxiaoyang05@meituan.com>
2017-01-05 23:07:17 +0800

    java quickSort

---
+++ Sort/QuickSort.java
@@ -0,0 +1,15 @@
+public class QuickSort {
+  public static void main(String[] args) {
+    int[] array = new int[]{2, 4, 3, 1, 5};
+  }
+
+  public static void quickSort(int[] sortedArray){
+    int i = 0;
+    int j = sortedArray.length;
+    int key = sortedArray[i];
+
+    while(i != j) {
+      while()
+    }
+  }
+}
